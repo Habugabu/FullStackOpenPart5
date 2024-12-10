8a55bbb024b86705c297edefd5f2367551859fa7
Habugabu <115887891+Habugabu@users.noreply.github.com>
2024-12-10 12:57:47 +0900

    5.1 completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,57 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
+import LoginForm from './components/LoginForm'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
       setBlogs( blogs )
     )  
   }, [])
+
+  const handleLogin = async (event) => {    
+    event.preventDefault()        
+    try {      
+      const user = await loginService.login({        
+        username, password,      
+      })      
+      setUser(user)      
+      setUsername('')      
+      setPassword('')    
+    } catch (exception) {      
+      setErrorMessage('Wrong credentials')      
+      setTimeout(() => {        
+        setErrorMessage(null)      
+      }, 5000)  
+    }
+  }
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value)
+  }
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value)
+  }
+
+  if (user === null){
+    return (
+      <LoginForm 
+        username={username} 
+        password={password} 
+        onLogin={handleLogin} 
+        onPasswordChange={handlePasswordChange} 
+        onUsernameChange={handleUsernameChange}
+      />
+    )
+  }
 
   return (
     <div>
---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,30 @@
+const LoginForm = ({onLogin, username, password, onUsernameChange, onPasswordChange}) => {
+    return (
+        <div>
+            <h2>login to application</h2>
+            <form onSubmit={onLogin}>
+                <div>
+                    username
+                    <input
+                        type='text'
+                        value={username}
+                        name='Username'
+                        onChange={onUsernameChange}
+                    />
+                </div>
+                <div>
+                    password
+                    <input
+                        type='text'
+                        value={password}
+                        name='Password'
+                        onChange={onPasswordChange}
+                    />
+                </div>
+                <button type='submit'>login</button>
+            </form>
+        </div>
+    )
+}
+
+export default LoginForm
