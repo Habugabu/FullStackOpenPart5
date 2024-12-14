f721370dd8f4a400aaa62fa3465460c15fcbb27c
Habugabu <115887891+Habugabu@users.noreply.github.com>
2024-12-14 14:30:36 +0900

    5.7 completed

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,35 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import {useState} from 'react'
+
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+
+  const hideWhenVisible = { display: visible ? 'none' : '' }
+  const showWhenVisible = { display: visible ? '' : 'none' }
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return (
+    <div style={blogStyle}>
+      {blog.title} {blog.author}
+      <button style={hideWhenVisible} onClick={toggleVisibility}>view</button>
+      <button style={showWhenVisible} onClick={toggleVisibility}>hide</button>
+      <div style={showWhenVisible}>
+        {blog.url}<br />
+        likes: {blog.likes}<button>like</button><br />
+        {blog.user.name}<br />
+      </div>
+    </div>
+  )
+}
 
 export default Blog
