95c6c07edf1fe887be27f6005808e65203868af9
Habugabu <115887891+Habugabu@users.noreply.github.com>
2025-01-06 20:32:22 +0900

    run Prettier

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,41 +1,51 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 const Blog = ({ user, blog, onLike, onDelete }) => {
-  const [visible, setVisible] = useState(false)
+  const [visible, setVisible] = useState(false);
 
-  const hideWhenVisible = { display: visible ? 'none' : '' }
-  const showWhenVisible = { display: visible ? '' : 'none' }
-  const deletable = { display: user.username === blog.user.username ? '' : 'none' }
+  const hideWhenVisible = { display: visible ? "none" : "" };
+  const showWhenVisible = { display: visible ? "" : "none" };
+  const deletable = {
+    display: user.username === blog.user.username ? "" : "none",
+  };
 
   const toggleVisibility = () => {
-    setVisible(!visible)
-  }
+    setVisible(!visible);
+  };
 
   const blogStyle = {
     paddingTop: 10,
     paddingLeft: 2,
-    border: 'solid',
+    border: "solid",
     borderWidth: 1,
-    marginBottom: 5
-  }
+    marginBottom: 5,
+  };
 
   return (
-    <div style={blogStyle} className='blog'>
+    <div style={blogStyle} className="blog">
       <i>{blog.title}</i> {blog.author}
-      <button style={hideWhenVisible} onClick={toggleVisibility}>view</button>
-      <button style={showWhenVisible} onClick={toggleVisibility}>hide</button>
+      <button style={hideWhenVisible} onClick={toggleVisibility}>
+        view
+      </button>
+      <button style={showWhenVisible} onClick={toggleVisibility}>
+        hide
+      </button>
       <div style={showWhenVisible}>
-        {blog.url}<br />
+        {blog.url}
+        <br />
         likes: {blog.likes}
-        <button
-          onClick={onLike}
-          id={blog.id}
-        >like</button><br />
-        {blog.user.name}<br />
-        <button style={deletable} onClick={onDelete} id={blog.id}>delete</button>
+        <button onClick={onLike} id={blog.id}>
+          like
+        </button>
+        <br />
+        {blog.user.name}
+        <br />
+        <button style={deletable} onClick={onDelete} id={blog.id}>
+          delete
+        </button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Blog
+export default Blog;
